7e2add256eb8ab54bbe3cc8375ab4f90628aac47
Ronobir Das <10803708+RonobirDas@users.noreply.github.com>
2025-01-06 03:28:55 -0800

    working on duplicate row issues

--- Notebooks/sourcetableEDA.py
+++ Notebooks/sourcetableEDA.py
@@ -73,9 +73,26 @@ def _(TotalElectricityProductionRaw, mo):
     return
 
 
+@app.cell
+def _(WindProductionRaw, landing, mo):
+    _df = mo.sql(
+        f"""
+        SELECT *
+        FROM landing.WindProductionRaw
+        WHERE startTime not in (SELECT endTime from landing.WindProductionRaw)
+        UNION 
+        SELECT *
+        FROM landing.WindProductionRaw
+        WHERE endTime not in (SELECT startTime from landing.WindProductionRaw)
+        ORDER BY startTime
+        """
+    )
+    return
+
+
 @app.cell(hide_code=True)
 def _(mo):
-    mo.md(r"""## Wind Energy Production """)
+    mo.md(r"""## Wind Energy Production""")
     return
 
 
@@ -104,6 +121,71 @@ def _(WindProductionRaw, mo):
     _df = mo.sql(
         f"""
         SELECT * FROM WindProductionRaw USING SAMPLE 100 ORDER BY startTime;
+        """
+    )
+    return
+
+
+@app.cell
+def _(WindProductionRaw, mo):
+    _df = mo.sql(
+        f"""
+        SELECT * FROM WindProductionRaw ORDER BY startTime LIMIT 100 OFFSET 100000;
+        """
+    )
+    return
+
+
+@app.cell
+def _(WindProductionRaw, landing, mo):
+    _df = mo.sql(
+        f"""
+        WITH x AS (
+           SELECT
+              *,
+              CASE
+                 WHEN startTime = endTime THEN endTime - INTERVAL '3 MINUTES'
+                 ELSE startTime
+              END AS adj_start_time,
+              LAG(endTime) OVER (ORDER BY endTime) > startTime AS overlap_with_previous,
+              LEAD(startTime) OVER (ORDER BY endTime) < endTime AS overlap_with_next
+           FROM landing.WindProductionRaw
+           ORDER BY endTime
+        )
+        SELECT *
+        FROM x
+        WHERE overlap_with_previous OR overlap_with_next;
+        """
+    )
+    return
+
+
+@app.cell
+def _(WindProductionRaw, landing, mo, overlap):
+    _df = mo.sql(
+        f"""
+        WITH adj_time AS (
+           SELECT
+              *,
+              CASE
+                 -- assume the previous record's endTime as startTime
+                 WHEN startTime = endTime THEN LAG(endTime) OVER (ORDER BY endTime)
+                 ELSE startTime
+              END AS adj_start_time
+           FROM landing.WindProductionRaw
+           ORDER BY endTime
+        )
+        , overlap AS (
+          SELECT
+              *,
+              LAG(endTime) OVER (ORDER BY endTime) > adj_start_time AS overlap_with_previous,
+              LEAD(adj_start_time) OVER (ORDER BY endTime) < endTime AS overlap_with_next
+              FROM adj_time
+        )
+        SELECT *
+        FROM overlap
+        WHERE overlap_with_previous OR overlap_with_next
+        ORDER BY endTime;
         """
     )
     return
@@ -139,7 +221,7 @@ def _(NuclearProductionRaw, mo):
 def _(NuclearProductionRaw, mo):
     _df = mo.sql(
         f"""
-        SELECT * FROM NuclearProductionRaw USING SAMPLE 100 ORDER BY startTime; 
+        SELECT * FROM NuclearProductionRaw USING SAMPLE 100 ORDER BY startTime;
         """
     )
     return
@@ -175,7 +257,7 @@ def _(HydroProductionRaw, mo):
 def _(HydroProductionRaw, mo):
     _df = mo.sql(
         f"""
-        SELECT * FROM HydroProductionRaw USING SAMPLE 100 ORDER BY startTime; 
+        SELECT * FROM HydroProductionRaw USING SAMPLE 100 ORDER BY startTime;
         """
     )
     return
@@ -211,7 +293,7 @@ def _(HydroProductionRaw, mo):
 def _(HydroProductionRaw, mo):
     _df = mo.sql(
         f"""
-        SELECT * FROM HydroProductionRaw USING SAMPLE 100 ORDER BY startTime; 
+        SELECT * FROM HydroProductionRaw USING SAMPLE 100 ORDER BY startTime;
         """
     )
     return
